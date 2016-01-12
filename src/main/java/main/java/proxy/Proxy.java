a3c43e4ccaaec407dfc05f10ef00a752a121fd73
Joakim Lindquister <joakiml@gmail.com>
2016-01-12 12:30:32 +0100

    Refactor into smaller methods.

--- src/main/java/main/java/proxy/Proxy.java
+++ src/main/java/main/java/proxy/Proxy.java
@@ -1,31 +1,59 @@
 package main.java.proxy;
 
-import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
 
 public class Proxy {
+
+    private final DefaultCamelContext camelContext;
+    private int listenPort;
+    private int proxyPairPort;
+
     public static void main(String args[]) {
 
+        if (args.length < 2) {
+            printUsage();
+            System.exit(1);
+        }
+
         final int listenPort = Integer.parseInt(args[0]);
-        final int forwardPort = Integer.parseInt(args[1]);
+        final int proxyPairPort = Integer.parseInt(args[1]);
 
-        final String incomingServiceMessagePath = "jetty:http://localhost:" + listenPort+ "/helloworld?wsdl";
-        final String proxyPath = "http://localhost:" + forwardPort + "/proxy/incoming?bridgeEndpoint=true";
-        final String incomingProxyMessagePath = "jetty:http://localhost:" + listenPort + "/proxy/incoming";
-        final String outgoingServicePath = "jetty:http://localhost:" + listenPort + "/proxy/incoming";
+        new Proxy(listenPort, proxyPairPort).start();
+    }
 
-        CamelContext context = new DefaultCamelContext();
+
+    public Proxy(int listenPort, int proxyPairPort) {
+        this.listenPort = listenPort;
+        this.proxyPairPort = proxyPairPort;
+        this.camelContext = new DefaultCamelContext();
+        buildRoutes();
+    }
+
+    public void start() {
+        try {
+            camelContext.start();
+            System.out.println("Proxy listening on port " + listenPort);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void buildRoutes() {
+        final String incomingServiceMessagePath = "jetty:http://localhost:" + listenPort + "/ws?matchOnUriPrefix=true";
+        final String proxyPath = "http://localhost:" + proxyPairPort + "/proxy/incoming?bridgeEndpoint=true&throwExceptionOnFailure=false";
+        final String incomingProxyMessagePath = "jetty:http://localhost:" + listenPort + "/proxy/incoming/?matchOnUriPrefix=true";
+        final String outgoingServicePath = "jetty:http://localhost:" + "1337" + "/ws/hello?bridgeEndpoint=true&hrowExceptionOnFailure=false";
 
         try {
-            context.addRoutes(new RouteBuilder() {
+            camelContext.addRoutes(new RouteBuilder() {
                 public void configure() {
                     from(incomingServiceMessagePath).process(new IncomingServiceMessageProcessor())
                             .to(proxyPath);
                 }
             });
-            context.addRoutes(new RouteBuilder() {
+            camelContext.addRoutes(new RouteBuilder() {
                 public void configure() {
                     from(incomingProxyMessagePath).process(new IncomingProxyMessageProcessor())
                             .to(outgoingServicePath);
@@ -35,12 +63,9 @@ public class Proxy {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
-        try {
-            context.start();
-            System.out.println("Proxy listening on port " + listenPort);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private static void printUsage() {
+       System.out.println("Usage: proxy listenPort proxyPairPort");
     }
 }
