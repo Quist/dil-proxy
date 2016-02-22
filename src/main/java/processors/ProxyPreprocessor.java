4710750543b52bd2c94d92b061f29aa95619147e
Joakim Lindquister <joakiml@gmail.com>
2016-02-22 17:37:50 +0100

    Log when using proxy pre processor

--- src/main/java/processors/ProxyPreprocessor.java
+++ src/main/java/processors/ProxyPreprocessor.java
@@ -3,13 +3,18 @@ package processors;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.http.common.HttpMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 
 public class ProxyPreprocessor implements Processor {
+    private final Logger logger = LoggerFactory.getLogger(ProxyPreprocessor.class);
 
     @Override
     public void process(Exchange exchange) throws Exception {
+        logger.info("Starting pre processing of exchange before sending to other proxy.");
+
         HttpServletRequest req = exchange.getIn(HttpMessage.class).getRequest();
         exchange.getIn().setHeader("path", req.getRequestURL());
         String headers = "{\"path\":\"" + req.getRequestURL() + "\"}";
