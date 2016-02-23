bdffcf0aa5ab5a0493e72f4e87d0ee44ef1df279
Joakim Lindquister <joakiml@gmail.com>
2016-02-23 11:40:07 +0100

    Fix config for AMQP

--- src/main/java/config/DilProxyConfig.java
+++ src/main/java/config/DilProxyConfig.java
@@ -2,9 +2,7 @@ package config;
 
 import com.typesafe.config.Config;
 import proxy.Protocol;
-
 import java.util.Optional;
-
 import static proxy.Protocol.*;
 
 public class DilProxyConfig {
@@ -25,7 +23,8 @@ public class DilProxyConfig {
         targetProxyHostname = proxyConfig.getString("targetProxyHostname");
         hostname = proxyConfig.getString("hostname");
 
-        this.selectedProtocol = setProxyConfig(proxyConfig);
+        selectedProtocol = getProtocol(proxyConfig);
+        setProtocolConfig(selectedProtocol, config);
     }
 
     public boolean useCompression() {
@@ -62,10 +61,9 @@ public class DilProxyConfig {
         return mqttConfig.get();
     }
 
-    private Protocol setProxyConfig(Config config) {
-        switch (config.getString("protocol").toLowerCase()) {
+    private Protocol getProtocol(Config proxyConfig) {
+        switch (proxyConfig.getString("protocol").toLowerCase()) {
             case "amqp": {
-                this.amqpConfig = Optional.of(new AmqpConfig(config));
                 return AMQP;
             }
             case "http": {
@@ -78,7 +76,16 @@ public class DilProxyConfig {
                 return COAP;
             }
             default: {
-                throw  new IllegalArgumentException("Unsupported protocol: " + config.getString("protocol"));
+                throw  new IllegalArgumentException("Unsupported protocol: " + proxyConfig.getString("protocol"));
+            }
+        }
+    }
+
+    private void setProtocolConfig(Protocol protocol, Config config) {
+        switch (protocol) {
+            case AMQP: {
+                this.amqpConfig = Optional.of(new AmqpConfig(config.getConfig("amqp")));
+                break;
             }
         }
     }
