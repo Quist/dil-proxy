1d1cf5e2e70147d6947176481fed9755f31878ed
Joakim Lindquister <joakiml@gmail.com>
2016-03-03 17:33:42 +0100

    Remove Camel header.
    The Camel HTTP component appended this header to the target URL, causing
    wrong request url at the target Web service.

--- src/main/java/processors/protocols/HttpRequest.java
+++ src/main/java/processors/protocols/HttpRequest.java
@@ -1,15 +1,27 @@
 package processors.protocols;
 
+import org.apache.camel.http.common.HttpMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import util.HttpRequestLogger;
+
+import javax.servlet.http.HttpServletRequest;
 
 public class HttpRequest implements Processor {
     private final Logger logger = LoggerFactory.getLogger(HttpRequest.class);
 
     @Override
     public void process(Exchange exchange) throws Exception {
+        removeCamelRoutingHeaders(exchange);
+
         logger.info("Received an incoming HTTP request message from the other proxy.");
+        HttpServletRequest request = exchange.getIn(HttpMessage.class).getRequest();
+        new HttpRequestLogger().log(request, null);
+    }
+
+    private void removeCamelRoutingHeaders(Exchange exchange) {
+        exchange.getIn().removeHeader("CamelHttpPath");
     }
 }
